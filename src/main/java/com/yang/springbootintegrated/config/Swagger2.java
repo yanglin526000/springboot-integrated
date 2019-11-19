21335b2c82bec48194e7ca5334157663f0fb4185
木木 <893713339@qq.com>
2019-11-19 17:34:49 +0800

    更新Swagger2配置，更加通用，禁用后页面更加友好

--- src/main/java/com/yang/springbootintegrated/config/Swagger2.java
+++ src/main/java/com/yang/springbootintegrated/config/Swagger2.java
@@ -4,8 +4,10 @@ import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -20,78 +22,107 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @description Swagger2配置文件
- * @classname Swagger2
- *
+ * <p>
+ * 描述: Swagger2配置文件
+ * </p>
+ * <p>
+ * 创建时间: 2019-11-15 09:50
+ * </p>
+ * 
  * @author yanglin
- * @time 2019-03-21 16:55:07.190 +0800
- * @version v1.0
  */
 @Configuration
 @EnableSwagger2
 public class Swagger2 {
 
-	/**
-	 * @description 创建访问路径
-	 * @return Docket
-	 * 
-	 * @author yanglin
-	 * @time 2019-03-21 16:55:21.290 +0800
-	 */
-	@Bean
-	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("com.wonders.pathcloudserver.controller"))
-				.paths(PathSelectors.regex("^(?!auth).*$")).build().securitySchemes(securitySchemes())
-				.securityContexts(securityContexts());
-	}
+    @Value("${swagger.enable:true}")
+    private Boolean enable;
 
-	/**
-	 * @description Swagger2主信息
-	 * @return ApiInfo
-	 * 
-	 * @author yanglin
-	 * @time 2019-03-21 16:55:49.338 +0800
-	 */
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("path-cloud-server").description("springboot利用swagger构建api文档")
-				.termsOfServiceUrl("").version("1.0").build();
-	}
+    /**
+     * <p>
+     * 描述: Docket
+     * </p>
+     * <p>
+     * 创建时间: 2019-11-15 09:53
+     * </p>
+     * 
+     * @return Docket
+     * 
+     * @author yanglin
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).enable(enable).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+                .build().securitySchemes(securitySchemes()).securityContexts(securityContexts());
+    }
 
-	/**
-	 * @description 配置了哪些操作与SecuritySchemes相关联的配置
-	 * @return List<ApiKey>
-	 * 
-	 * @author yanglin
-	 * @time 2019-03-21 16:55:58.426 +0800
-	 */
-	private List<ApiKey> securitySchemes() {
-		return newArrayList(new ApiKey("token", "token", "header"));
-	}
+    /**
+     * <p>
+     * 描述: Swagger2主信息
+     * </p>
+     * <p>
+     * 创建时间: 2019-11-15 09:53
+     * </p>
+     * 
+     * @return ApiInfo
+     * 
+     * @author yanglin
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("wonders-pathology-server").description("springboot利用swagger构建api文档")
+                .termsOfServiceUrl("").version("1.0").build();
+    }
 
-	/**
-	 * @description 配置api操作（通过正则表达式模式）和HTTP方法将安全上下文应用于apis
-	 * @return List<SecurityContext>
-	 * 
-	 * @author yanglin
-	 * @time 2019-03-21 16:56:14.905 +0800
-	 */
-	private List<SecurityContext> securityContexts() {
-		return newArrayList(SecurityContext.builder().securityReferences(defaultAuth())
-				.forPaths(PathSelectors.regex("^(?!auth).*$")).build());
-	}
+    /**
+     * <p>
+     * 描述: 配置了哪些操作与SecuritySchemes相关联的配置
+     * </p>
+     * <p>
+     * 创建时间: 2019-11-15 09:53
+     * </p>
+     * 
+     * @return List<ApiKey>
+     * 
+     * @author yanglin
+     */
+    private List<ApiKey> securitySchemes() {
+        return newArrayList(new ApiKey("token", "token", "header"));
+    }
 
-	/**
-	 * @description 默认的认证信息
-	 * @return List<SecurityReference>
-	 * 
-	 * @author yanglin
-	 * @time 2019-03-21 16:56:41.595 +0800
-	 */
-	private List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return newArrayList(new SecurityReference("token", authorizationScopes));
-	}
+    /**
+     * <p>
+     * 描述: 配置api操作（通过正则表达式模式）和HTTP方法将安全上下文应用于apis
+     * </p>
+     * <p>
+     * 创建时间: 2019-11-15 09:54
+     * </p>
+     * 
+     * @return List<SecurityContext>
+     * 
+     * @author yanglin
+     */
+    private List<SecurityContext> securityContexts() {
+        return newArrayList(SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("^(?!auth).*$")).build());
+    }
+
+    /**
+     * <p>
+     * 描述: List<SecurityReference>
+     * </p>
+     * <p>
+     * 创建时间: 2019-11-15 09:54
+     * </p>
+     * 
+     * @return List<SecurityReference>
+     * 
+     * @author yanglin
+     */
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(new SecurityReference("token", authorizationScopes));
+    }
 }
